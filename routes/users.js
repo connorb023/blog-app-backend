601aab2616c4dca2527bb148f5943eed9459c672
connorb023 <connorcb@yahoo.com>
2023-05-22 15:39:40 -0600

    user route set up

---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
+
+
+module.exports = router;
