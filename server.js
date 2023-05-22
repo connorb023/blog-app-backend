cd0db37934483b208adf78b4a6115687ee2afe0a
connorb023 <connorcb@yahoo.com>
2023-05-22 15:53:03 -0600

    multer template

--- server.js
+++ server.js
@@ -22,3 +22,18 @@ mongoose
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+  
