00cd621ab842a517b05a7e37e1b4a1b6bd35337c
connorb023 <connorcb@yahoo.com>
2023-05-23 06:51:48 -0600

    fixes

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
 app.use(express.json());
@@ -35,12 +32,21 @@ mongoose
   app.post("/api/upload", upload.single("file"), (req, res) => {
     res.status(200).json("File has been uploaded");
   });
-  
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
+  // Middleware
+app.use(express.json());
+app.use(cors()); // Enable CORS
+  
+  // Routes
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const postsRoute = require("./routes/posts");
+const categoriesRoute = require("./routes/categories");
+
+  app.use("/auth", authRoute);
+  app.use("/users", usersRoute);
+  app.use("/posts", postsRoute);
+  app.use("/categories", categoriesRoute);
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
